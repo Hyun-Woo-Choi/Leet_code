5d4cad1cf3d735e8f607d219d9c757d2db161893
Hyun Woo(David) <122449275+Hyun-Woo-Choi@users.noreply.github.com>
2026-09-07 23:25:18 -0700

    Time: 3 ms (88.92%), Space: 20.1 MB (33.93%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,24 +1,12 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        n = len(nums)
-        position = 0
-        move = 0
-
-        while position < n - 1:
-            distance = nums[position]
-
-            # 여기서 바로 끝까지 갈 수 있으면 한 번이면 끝
-            if position + distance >= n - 1:
-                return move + 1
-
-            best_reach = -1
-            nxt = position
-            for i in range(position + 1, position + distance + 1):
-                if i + nums[i] > best_reach:
-                    best_reach = i + nums[i]
-                    nxt = i
-
-            position = nxt
-            move += 1
-
-        return move
+        jumps = 0
+        curEnd = 0
+        farthest = 0
+        for i in range(len(nums) -1):
+            farthest = max(farthest, i + nums[i])
+            if i == curEnd:
+                jumps += 1
+                curEnd = farthest
+        return jumps 
+        
